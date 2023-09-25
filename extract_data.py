b7a81d8f840eb331a5a7828a7062f76adc5c7826
Moustafa Ismail <mostafaismael10@hotmail.com>
2023-09-25 20:14:59 +0000

    Results added

--- extract_data.py
+++ extract_data.py
@@ -9,18 +9,17 @@ file_paths = [f"results/{project}/{file}" for file in ["hrps.txt", "mrps.txt", "
 
 # Define the patterns to match
 info_pattern = "INFO: (.+: \d+)"
-time_pattern = r"\[INFO\] Total time: ([\d.:]+\s*\w+)"
+time_pattern = "\[INFO\] Total time:(.*)"
 results_str = ""
 
 # Process each file
 for file_path in file_paths:
     with open(file_path, 'r') as file:
-        # data = file.read().split("=====")
+        # The weird splitting below is to curate the relevant data that corresponds to one commit
         data = file.read().split("================================")
         data = "\n".join(data).strip()
-        # data = data.split("=====")
         data = [item for item in data.split("=====") if item]
-        i = 1
+        i = 1 # Commit counter (placeholder for commit SHA)
         for commit in data:
             results_str = f"{project}:{i}:{file_path}: "
             i+=1
